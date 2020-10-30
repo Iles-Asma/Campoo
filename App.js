5a237391f101cae491f2bc7d9cfffc4b1df5899b
Iles-Asma <60738751+Iles-Asma@users.noreply.github.com>
2020-10-30 17:36:38 +0100

    test 1

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { render } from 'react-dom';
 import { StyleSheet, Text, View } from 'react-native';
+import ScreenLoginPage from './app/screen/ScreenLoginPage';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+
+    return (
+
+     <ScreenLoginPage />
+
+    );
+
+  }
+
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- app/screen/ScreenLoginPage.js
+++ app/screen/ScreenLoginPage.js
@@ -1,1 +1,33 @@
 
+import React from "react";
+import {  View, Text, StyleSheet } from "react-native";
+
+export default class ScreenLoginPage extends React.Component {
+    render(){
+
+        return(
+            <View style={styles.container}>
+                <Text>test1 osudhfwioubiauhijhbagwe</Text>
+
+            </View>
+             
+
+
+        );
+
+
+    }
+}
+
+const styles = StyleSheet.create({
+   container : {
+      flex : 1,
+      backgroundColor :'white',
+ 
+      alignItems : 'center',
+      justifyContent :'center',
+    
+
+    
+   },
+});
