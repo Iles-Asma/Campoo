a65fea97b48782788c6d05dce099bd2c2d90b546
Iles-Asma <60738751+Iles-Asma@users.noreply.github.com>
2020-10-30 19:00:01 +0100

    Update ScreenLoginPage.js

--- app/screen/ScreenLoginPage.js
+++ app/screen/ScreenLoginPage.js
@@ -1,19 +1,35 @@
 
 import React from "react";
-import {  View, Text, StyleSheet } from "react-native";
+import {  View, Text,Button, StyleSheet , SafeAreaView,StatusBar, Platform, TextInput } from "react-native";
 
 export default class ScreenLoginPage extends React.Component {
     render(){
+       return(
+          <SafeAreaView style={styles.container} >
+           
+            <View style={styles.loginview}>
 
-        return(
-            <View style={styles.container}>
-                <Text>test1 osudhfwioubiauhijhbagwe</Text>
+               <View style={styles.Inputview}>
+                  <Text>Pseudo</Text>
+                   <TextInput style={styles.textinput}/>
+               </View> 
+
+               <View style={styles.Inputview}>
+                  <Text>Mot de passe</Text>
+                   <TextInput style={styles.textinput}/>
+               </View> 
+
+
+
+
 
             </View>
+
+          </SafeAreaView>
              
 
 
-        );
+       );
 
 
     }
@@ -23,11 +39,48 @@ const styles = StyleSheet.create({
    container : {
       flex : 1,
       backgroundColor :'white',
- 
-      alignItems : 'center',
+      paddingTop : Platform.OS === "android" ? StatusBar.currentHeight: 0,
+      borderWidth : 3,
+      borderColor: 'green',
       justifyContent :'center',
-    
-
-    
+      alignItems : 'center',
+     
    },
+
+  loginview: {
+      
+       borderWidth : 3,
+       borderColor :'blue',
+       width : '95%',
+       height: '80%',  
+   },
+
+
+
+   Inputview:{
+       borderColor: 'black',
+       borderWidth : 3,
+       margin :10,
+       flex: 0.1,
+       flexDirection :'column',
+       justifyContent : 'flex-start',
+       height:40,
+             
+
+
+   },
+
+
+   textinput : {
+      
+       height : 40,
+       borderWidth : 2,
+       borderColor : 'red',
+       width: '100%',     
+
+   }
+
+
+
+
 });
