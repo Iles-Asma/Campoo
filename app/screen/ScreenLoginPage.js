d87ac6d0ca1657ffe4855e1d8f4daf27005261c5
Iles-Asma <60738751+Iles-Asma@users.noreply.github.com>
2020-10-31 21:40:14 +0100

    .

--- app/screen/ScreenLoginPage.js
+++ app/screen/ScreenLoginPage.js
@@ -1,11 +1,15 @@
 
 import React from "react";
-import {  TouchableOpacity,View, Text,Button, StyleSheet , SafeAreaView,StatusBar, Platform, TextInput } from "react-native";
+import {  TouchableOpacity,View, Text,Button, StyleSheet , SafeAreaView,StatusBar, Platform, TextInput,Image } from "react-native";
 
 export default class ScreenLoginPage extends React.Component {
     render(){
        return(
           <SafeAreaView style={styles.container} >
+
+             <Image style={{width:300, height:300,  left:-110.16, top :10, }} source={require("../../assets/images/Poulpe.png")}/>
+             <Image style={{width:500, height:500,   left:125.16, top :30,  zIndex:-5,  }} source={require("../../assets/images/Baleine.png")}/>
+             <Image style={{width:100, height:100, left:275,top:-780, }} source={require("../../assets/images/logoCampoo.png")}/>
            
             <View style={styles.loginview}>
 
@@ -13,7 +17,7 @@ export default class ScreenLoginPage extends React.Component {
 
                   <Text   style={styles.pseudoMdp} >Pseudo</Text>
 
-                   <TextInput  style={styles.textinput} label='username' placeholder={'pseudonyme'} maxLength={15}
+                   <TextInput  style={styles.textinput} label='username'  maxLength={15}
                    
                    />
 
@@ -23,7 +27,7 @@ export default class ScreenLoginPage extends React.Component {
 
                   <Text style={styles.pseudoMdp} >Mot de passe</Text>
 
-                   <TextInput style={styles.textinput} label='password' placeholder={'mot de passe'} maxLength={15} />
+                   <TextInput style={styles.textinput} label='password'  maxLength={15} />
 
                </View> 
 
@@ -32,7 +36,13 @@ export default class ScreenLoginPage extends React.Component {
                   <TouchableOpacity title={'Connexion'}  style={{ color: "white", borderRadius:5,  width:150, height:50, backgroundColor: '#4D3D64', marginTop:30, marginBottom:28, margin:75,}} >
                      
                      <Text style={styles.connexionText}>Connexion</Text>
+
+                  </TouchableOpacity>
+
+                  <TouchableOpacity title={"S'inscrire"}   >
                      
+                     <Text style={styles.register}>S'inscrire</Text>
+
                   </TouchableOpacity>
                  
              
@@ -49,28 +59,26 @@ export default class ScreenLoginPage extends React.Component {
 const styles = StyleSheet.create({
    container : {
       flex : 1,
-
       paddingTop : Platform.OS === "android" ? StatusBar.currentHeight: 0,
-      borderWidth : 3,
-      borderColor: 'green',
-    
-      alignItems:'center',
-      justifyContent:'center',
       flexDirection: 'column',
-     
+      
    },
 
   loginview: {
-
+     alignSelf:'center',
+       top:-660,
        width : 300,
        height: 270,  
+       zIndex:5,
+       
     
    },
 
    Inputview1:{
-   
+      
        width :"100%",
        marginBottom:20,
+       
     
    },
 
@@ -81,9 +89,10 @@ const styles = StyleSheet.create({
   },
 
    textinput : {
-      
+       paddingHorizontal:10,
+       textAlignVertical:'center',
        height : 30,
-       borderWidth : 2,
+       borderWidth : 1.5 ,
        borderColor : '#4D3D64',
        width :"100%",
        borderRadius:5,  
@@ -103,7 +112,15 @@ const styles = StyleSheet.create({
       fontSize:16,
       color:'white',
 
-   }
+   },
+
+   register:{ 
+      alignSelf:'center',
+      marginVertical:-20,
+      color:'#62BBC5',
+      fontSize:15,
+
+   },
 
  
 
