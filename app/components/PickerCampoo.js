8fc9058babf2ae52d949279089d6c39339b7c400
Iles-Asma <60738751+Iles-Asma@users.noreply.github.com>
2020-12-31 01:46:49 +0100

    Create PickerCampoo.js

---
+++ app/components/PickerCampoo.js
@@ -0,0 +1,46 @@
+// import React from "react";
+// import { View,  StyleSheet } from "react-native";
+//import {Picker} from '@react-native-picker/picker';
+
+//  npm install @react-native-picker/picker --save PS : sa fait crache le projet apres l'installation 
+// Erreur affiche : "super expression must either be null or a function"
+
+
+// export default class PickerCampoo extends React.Components {
+
+//     state = {
+//         question: 'Dans quelle ville est né votre père ?'
+//       };
+
+
+   
+//     render(){
+
+       
+//         return(
+            
+        
+
+          
+//         <Picker
+//             selectedValue={this.state.question}
+//             style={{height: 50, width: 100}}
+//             onValueChange={(itemValue, itemIndex) => this.setState({question: itemValue})}>
+//             <Picker.Item label="ville de naissance" value="Dans quelle ville est né votre père ?" />
+//             <Picker.Item label="text" value="text" />
+//         </Picker>
+//         );
+
+
+
+//     }
+// }
+
+// const styles = StyleSheet.create({
+//     container: {
+//       flex: 1,
+//       paddingTop: 40,
+//       alignItems: "center"
+//     }
+//   });
+
