3af26d1967ed9241ad73ea6a892ec31994c855c2
Iles-Asma <60738751+Iles-Asma@users.noreply.github.com>
2021-03-27 14:56:31 +0100

    ajustement

--- app/screens/signup/DobSignupCampoo.js
+++ app/screens/signup/DobSignupCampoo.js
@@ -6,108 +6,100 @@ import ButtonCampoo from "../../components/button/ButtonCampoo";
 import LabelCampoo from '../../components/LabelCampoo';
 import SecondaryButtonCampoo from '../../components/button/SecondaryButtonCampoo';
 import LogoCampoo from '../../../assets/svg/LogoCampoo'
-import PickerDate from '../../components/PickerDate';
+
 
 
 export default function DobSignupCampoo(props) {
-    const [birthday, setBirthday] = useState('');
+	const [birthday, setBirthday] = useState('');
 
-    const [errorMessage, setErrorMessage] = useState('');
-    console.log(birthday);
+	const [errorMessage, setErrorMessage] = useState('');
+	console.log(birthday);
 
-    const _retrieveData = async () => {
-        try {
-            const value = await AsyncStorage.getItem('token');
-            if (value !== null) {
-                // We have data!!
-                return value;
-            }
-        } catch (error) {
+	const _retrieveData = async () => {
+		try {
+			const value = await AsyncStorage.getItem('token');
+			if (value !== null) {
+				// We have data!!
+				return value;
+			}
+		} catch (error) {
 
-            // Error retrieving data
-        }
-    };
-
-
-    const onSubmit = async () => {
-
-        const token = await _retrieveData();
-        // console.log(token);
+			// Error retrieving data
+		}
+	};
 
 
+	const onSubmit = async () => {
 
-        fetch("https://campoo.fr/api/account/birthday", {
-            method: 'PATCH',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                "Authorization": `Bearer ${token}`
-
-            },
-            body: JSON.stringify({
-
-                'birthday': birthday
-
-            })
-        })
-            // response.json()
-            .then((response) => response.json())
-            .then((Message) => {
-
-                console.log(Message);
-                if (Message.Status === 'Success') {
-
-                    props.navigation.navigate('MdpSignupCampoo');
-
-                } else {
-
-                    setErrorMessage(Message.Message.birthday[0]);
-
-                }
-            })
-            .catch((error) => {
-                // console.error(error);
-            });
-
-
-    }
+		const token = await _retrieveData();
+		// console.log(token);
 
 
 
-    //! Mettre un picker
+		fetch("https://campoo.fr/api/account/birthday", {
+			method: 'PATCH',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json',
+				"Authorization": `Bearer ${token}`
+
+			},
+			body: JSON.stringify({
+
+				'birthday': birthday
+
+			})
+		})
+			// response.json()
+			.then((response) => response.json())
+			.then((Message) => {
+
+				console.log(Message);
+				if (Message.Status === 'Success') {
+
+					props.navigation.navigate('MdpSignupCampoo');
+
+				} else {
+
+					setErrorMessage(Message.Message.birthday[0]);
+
+				}
+			})
+			.catch((error) => {
+				// console.error(error);
+			});
+
+
+	}
 
 
 
-    return (
+	//! Mettre un picker
 
-        <SafeAreaView style={styles.container}>
 
-            {/* () => navigation.navigate('BatSignupCampoo') */}
 
-            <LogoCampoo style={{ right: '5%', top: 49, marginTop: 16, position: 'absolute', }} />
-            <Image style={styles.baloo} source={require("../../../assets/images/blob-baloo-fête.png")} />
+	return (
 
-            <View style={styles.dobView}>
-                <LabelCampoo style={styles.dobLabel}>Date de Naissance</LabelCampoo>
-                <Text style={styles.textDob}>Quand est-ce que Baloo doit te célébrer ?</Text>
+		<SafeAreaView style={styles.container}>
 
-                <PickerDate
-                    date={props.date}
-                    style={styles.InputView}
-                    errorText={errorMessage}
-                    value={props.birthday}
-                    onDateChange={(birthday) => { setBirthday(birthday) }}
+			{/* () => navigation.navigate('BatSignupCampoo') */}
 
-                />
+			<LogoCampoo style={{ right: '5%', top: 49, marginTop: 16, position: 'absolute', }} />
+			<Image style={styles.baloo} source={require("../../../assets/images/blob-baloo-fête.png")} />
 
-                <ButtonCampoo style={styles.button} onPress={onSubmit}>Suivant</ButtonCampoo>
-                <SecondaryButtonCampoo style={styles.retour} onPress={() => props.navigation.goBack()}>retour</SecondaryButtonCampoo>
+			<View style={styles.dobView}>
+				<LabelCampoo style={styles.dobLabel}>Date de Naissance</LabelCampoo>
+				<Text style={styles.textDob}>Quand est-ce que Baloo doit te célébrer ?</Text>
 
-            </View>
 
-        </SafeAreaView>
+				<ButtonCampoo style={styles.button} onPress={onSubmit}>Suivant</ButtonCampoo>
+				<SecondaryButtonCampoo style={styles.retour} onPress={() => props.navigation.goBack()}>retour</SecondaryButtonCampoo>
 
-    );
+			</View>
+
+		</SafeAreaView>
+
+	);
 
 
 
@@ -116,79 +108,79 @@ export default function DobSignupCampoo(props) {
 
 const styles = StyleSheet.create({
 
-    container: {
+	container: {
 
-        flex: 1,
+		flex: 1,
 
-        // test pour detecter la platform
-        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-        flexDirection: 'column',
-        backgroundColor: '#ffffff',
-
-
-    },
-
-    logoCampoo: {
-        position: 'absolute',
-        width: 115,
-        height: 115,
-        marginTop: 20,
-        left: 260
+		// test pour detecter la platform
+		paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+		flexDirection: 'column',
+		backgroundColor: '#ffffff',
 
 
-    },
+	},
 
-    InputView: {
-
-        alignSelf: 'center',
-
-    },
-
-    dobLabel: {
-        marginBottom: 9,
-
-    },
+	logoCampoo: {
+		position: 'absolute',
+		width: 115,
+		height: 115,
+		marginTop: 20,
+		left: 260
 
 
+	},
 
-    dobView: {
+	InputView: {
 
-        top: 138,
-        height: 290,
-        width: 300,
-        alignSelf: 'center',
+		alignSelf: 'center',
+
+	},
+
+	dobLabel: {
+		marginBottom: 9,
+
+	},
 
 
-    },
 
-    button: {
-        marginTop: 57,
-        alignSelf: 'center',
+	dobView: {
 
-    },
+		top: 138,
+		height: 290,
+		width: 300,
+		alignSelf: 'center',
 
-    baloo: {
-        position: "absolute",
-        width: 559,
-        height: 438,
-        left: 50,
-        top: 440,
-    },
 
-    textDob: {
+	},
 
-        fontSize: 11,
-        textAlign: 'left',
-        color: '#9B84D2',
+	button: {
+		marginTop: 57,
+		alignSelf: 'center',
 
-    },
+	},
 
-    retour: {
+	baloo: {
+		position: "absolute",
+		width: 559,
+		height: 438,
+		left: 50,
+		top: 440,
+	},
 
-        alignSelf: 'center',
-        marginTop: 10,
+	textDob: {
 
-    },
+		fontSize: 11,
+		textAlign: 'left',
+		color: '#9B84D2',
+
+	},
+
+	retour: {
+
+		alignSelf: 'center',
+		marginTop: 10,
+
+	},
 
 
 
