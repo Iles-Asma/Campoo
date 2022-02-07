91602b4f80e60d2c2d2fdda7c4c7cb2c46353941
Iles-Asma <iles.a@icloud.com>
2022-02-07 15:37:19 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,34 +1,14 @@
 import React from 'react';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
-
-
-
 import Navigations from './app/routes/Navigations'
 
-
-
-
 export default class App extends React.Component {
-
-
 	render() {
-
-
-
-
 		return (
-
-
-
-			<NavigationContainer >
+			<NavigationContainer>
 				<Navigations />
 			</NavigationContainer>
-
-
 		);
-
-
 	}
-
 }
